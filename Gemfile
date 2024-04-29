a588fd8a10cb0ad26390d30b9966ff4e36187539
tompng <tomoyapenguin@gmail.com>
2024-04-29 12:41:31 +0900

    bundle gem irb-theme-rk2024

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in irb-theme-rk2024.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/irb/theme/rk2024.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "rk2024/version"
+
+module Irb
+  module Theme
+    module Rk2024
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
